21cfa52e1ee90b91a0d911fe4d5c9d6fb6963790
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-10-03 00:25:05 +0900

    replaced folders, add test

---
+++ src/components/Button/Button.test.ts
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import { Button } from 'src/components/Button';
+import '@testing-library/jest-dom';
+
+describe('Button', () => {
+  test('renders a Button', () => {
+    // write some test at here
+  });
+});
